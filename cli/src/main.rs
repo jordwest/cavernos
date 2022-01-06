30fa9e48cecb5e3e0bece86208ee47fdf2f8361d
Jordan West <jordwest@gmail.com>
2022-01-06 20:10:01 +1000

    generates project from template

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,7 @@
 use structopt::StructOpt;
+use rust_embed::RustEmbed;
+use std::fs;
+use std::path::Path;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "new")]
@@ -17,7 +20,44 @@ enum Cli {
   New(NewOpts),
 }
 
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Asset;
+
+const SUPPORTED_LANGUAGES: [&'static str; 1] = ["rust"];
+
+fn new_command(opts: NewOpts) {
+  let project_name = opts.name;
+  
+  fs::create_dir(&project_name);
+  
+  for filename in Asset::iter() {
+    let path = Path::new(filename.as_ref());
+    if let Ok(path) = path.strip_prefix("templates") {
+      // Check it's for the language we want
+      if let Ok(path) = path.strip_prefix(&opts.language) {
+        println!("For language you chose {:?}", path);
+        
+        if let Some(embedded_file) = Asset::get(&filename) {
+          let path = Path::new(&project_name).join(path);
+
+          let containing_dir = path.parent().unwrap();
+          fs::create_dir(containing_dir);
+
+          fs::write(path, embedded_file.data);
+        } else {
+          panic!("Missing file {}", &filename)
+        }
+      }
+    }
+    println!("File {}", filename.as_ref());
+  }
+}
+
 fn main() {
-  let opts = Cli::from_args();
-  println!("Hello, world! {:#?}", opts);
+  let subcommand = Cli::from_args();
+  
+  match subcommand {
+    Cli::New(opts) => new_command(opts),
+  }
 }
