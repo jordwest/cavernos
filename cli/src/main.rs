076967f02f1b928d73a8ca2ef1d6c082a888be53
Jordan West <jordwest@gmail.com>
2022-01-07 10:38:06 +1000

    adds packaging command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,66 +1,18 @@
+mod subcommands;
+
 use structopt::StructOpt;
-use rust_embed::RustEmbed;
-use std::fs;
-use std::path::Path;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "new")]
-struct NewOpts {
-  #[structopt(name = "NAME")]
-  name: String,
-
-  #[structopt(short, long)]
-  language: String,
-}
+use subcommands::new::{NewOpts, new_command};
+use subcommands::package::{PackageOpts, package_command};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "cavernos")]
 enum Cli {
   #[structopt(name = "new")]
   New(NewOpts),
-}
-
-#[derive(RustEmbed)]
-#[folder = "assets/"]
-struct Asset;
-
-const SUPPORTED_LANGUAGES: [&'static str; 1] = ["rust"];
-
-fn new_command(opts: NewOpts) {
-  let project_name = opts.name;
   
-  fs::create_dir(&project_name);
-  
-  for filename in Asset::iter() {
-    let path = Path::new(filename.as_ref());
-    
-    // Extract common assets
-    if let Ok(path) = path.strip_prefix("common") {
-      let path_prefix = format!("{}/assets", &project_name);
-      let path = Path::new(&path_prefix).join(path);
-      
-      let containing_dir = path.parent().unwrap();
-      fs::create_dir(containing_dir);
-
-      let embedded_file = Asset::get(&filename).unwrap();
-      fs::write(path, embedded_file.data);
-    }
-
-    // Extract language specific files
-    if let Ok(path) = path.strip_prefix("templates") {
-      // Check it's for the language we want
-      if let Ok(path) = path.strip_prefix(&opts.language) {
-        let embedded_file = Asset::get(&filename).unwrap();
-        
-        let path = Path::new(&project_name).join(path);
-
-        let containing_dir = path.parent().unwrap();
-        fs::create_dir(containing_dir);
-
-        fs::write(path, embedded_file.data);
-      }
-    }
-  }
+  #[structopt(name = "package")]
+  Package(PackageOpts),
 }
 
 fn main() {
@@ -68,5 +20,11 @@ fn main() {
   
   match subcommand {
     Cli::New(opts) => new_command(opts),
+    Cli::Package(opts) => {
+      match package_command(opts) {
+        Ok(()) => (),
+        Err(msg) => eprintln!("Packaging error: {}", msg),
+      }
+    },
   }
 }
---
+++ cli/src/subcommands/package.rs
@@ -0,0 +1,86 @@
+use structopt::StructOpt;
+use std::fs;
+use std::path::Path;
+use serde_json::{Value, Map};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "package")]
+pub struct PackageOpts {
+  #[structopt(name = "MANIFEST")]
+  manifest_path: Option<String>,
+}
+
+const MAX_SUPPORTED_MANIFEST_VERSION: i64 = 1;
+
+/// Copies a file referenced in the source manifest to the dist directory, then updates
+/// the manifest with a new relative path.
+fn copy_and_update_path(map: &mut Map<String, Value>, manifest_dir: &Path, key: &str, new_path: &str) -> Result<(), String> {
+  let relative_path = map.get(key)
+    .ok_or(format!("Missing `{}` key in manifest", key))?
+    .as_str()
+    .ok_or(format!("Expected `{}` key to be a string", key))?;
+
+  let source_path = manifest_dir.join(relative_path);
+  
+  let destination_path = manifest_dir
+    .join("dist")
+    .join(Path::new(new_path));
+  
+  eprintln!("Copying {:?} --> {:?}", source_path, destination_path);
+  
+  fs::copy(&source_path, &destination_path)
+    .map_err(|_| format!("Could not copy {:?} to dist directory. Have you built the project?", &source_path))?;
+  
+  map.insert(key.to_string(), Value::String(new_path.to_string()));
+  
+  Ok(())
+}
+
+pub fn package_command(opts: PackageOpts) -> Result<(), String> {
+  let manifest_path = opts.manifest_path.unwrap_or("manifest.json".to_string());
+  let manifest_path = Path::new(&manifest_path);
+  let manifest_directory = manifest_path.parent().unwrap();
+  
+  let manifest_data = fs::read(manifest_path)
+    .map_err(|_| format!("Error reading manifest file. Are you in the right directory?"))?;
+  
+  let mut v: Value = serde_json::from_slice(&manifest_data)
+    .map_err(|e| format!("Error parsing manifest: {}", e))?;
+  
+  let dist_directory = manifest_directory.join(Path::new("dist"));
+  
+  fs::create_dir_all(&dist_directory);
+  
+  let manifest_dict = v.as_object_mut().ok_or("Invalid manifest. Expected a JSON object at the root.")?;
+  
+  let manifest_version = manifest_dict.get("manifestVersion")
+    .ok_or("Missing `manifestVersion` key")?
+    .as_i64()
+    .ok_or("Expected `manifestVersion` to be an integer")?;
+  
+  if manifest_version > MAX_SUPPORTED_MANIFEST_VERSION {
+    let cli_path = std::env::current_exe().unwrap();
+    let cli_name = cli_path.file_name().unwrap().to_str().unwrap();
+    eprintln!(
+      "WARNING: Manifest version {} not supported, things may not behave as expected. Maximum version supported is {}. Do you need to upgrade {}?",
+      manifest_version, MAX_SUPPORTED_MANIFEST_VERSION, cli_name
+    )
+  }
+  
+  copy_and_update_path(manifest_dict, manifest_directory, "program", "program.wasm")?;
+  copy_and_update_path(manifest_dict, manifest_directory, "palette", "palette.png")?;
+  
+  let fonts_dict = manifest_dict
+    .get_mut("fonts")
+    .ok_or("Manifest missing `fonts` key")?
+    .as_object_mut()
+    .ok_or("Expected `fonts` key to be a JSON object")?;
+  
+  copy_and_update_path(fonts_dict, manifest_directory, "narrow", "font_narrow.png")?;
+  copy_and_update_path(fonts_dict, manifest_directory, "square", "font_square.png")?;
+  
+  let manifest_data = serde_json::to_vec_pretty(manifest_dict).unwrap();
+  fs::write((&dist_directory).join("manifest.json"), manifest_data);
+  
+  Ok(())
+}
