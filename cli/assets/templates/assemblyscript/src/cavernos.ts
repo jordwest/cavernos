51380211021f1705a8e7d7855b520b3d5623bae1
Jordan West <jordwest@gmail.com>
2022-01-07 15:27:23 +1000

    barebones working assemblyscript template

---
+++ cli/assets/templates/assemblyscript/src/cavernos.ts
@@ -0,0 +1,8 @@
+
+export const COLS = 0;
+export const ROWS = 1;
+
+export const CHARS = 3072;
+export const FG_COLOR = 134144;
+
+export const OS = new ArrayBuffer(262144);
---
+++ cli/assets/templates/assemblyscript/src/main.ts
@@ -0,0 +1,18 @@
+import { COLS, ROWS, CHARS, FG_COLOR } from './cavernos';
+
+export const OS = new ArrayBuffer(262144);
+
+export function init(os_pointer: usize): usize {
+  store<u8>(os_pointer + COLS, 80);
+  store<u8>(os_pointer + ROWS, 24);
+  
+  return 0;
+}
+
+export function frame (os_pointer: usize, app_pointer: usize, dt: f64): void {
+  store<u8>(os_pointer + CHARS, 1);
+  store<u8>(os_pointer + CHARS + 1, 1);
+
+  store<u8>(os_pointer + FG_COLOR, 1);
+  store<u8>(os_pointer + FG_COLOR + 1, 1);
+}
